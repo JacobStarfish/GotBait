7da56050e609571acfa1d730d0c122830c4796c3
Jacob Smitherman <jacob@starfish.com>
2022-11-28 17:01:55 -0600

    HomeScreen

--- app/screens/HomeScreen/HomeScreen.js
+++ app/screens/HomeScreen/HomeScreen.js
@@ -1,31 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, ScrollView, useWindowDimensions } from 'react-native';
+import LiveCrab from 'assets/LiveCrab.jpg';
+import LiveCroaker from 'assets/LiveCroaker.jpg';
+import LiveShrimp from 'assets/LiveShrimp.jpeg';
 
 
 const HomeScreen = () => {
-  console.log("App Executed")
+  const { height } = useWindowDimensions();
+
   return (
-    <Text>Home Sweet Home</Text>
+    <ScrollView>
+      <Text style={styles.headerText}>Below is a list of Live Bait and their Availability</Text>
+
+      <Text>Live Shrimp</Text>
+      <Image
+        source={LiveShrimp}
+        styles={{height: height * 0.3}}
+        resizeMode='contain'
+      />
+      <Text style={styles.availableText}>Available</Text>
+
+      <Text>Live Croaker</Text>
+      <Image
+        source={LiveCroaker}
+        styles={{height: height * 0.3}}
+        resizeMode='contain'
+      />
+      <Text style={styles.availableText}>Available</Text>
+
+      <Text>Live Crab</Text>
+      <Image
+        source={LiveCrab}
+        styles={{height: height * 0.3}}
+        resizeMode='contain'
+      />
+      <Text style={styles.availableText}>Available</Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, //Grows and fits to entire screen. 1 = 100%
-    //backgroundColor: 'darkolivegreen', 
+  headerText: {
+    fontSize: 16,
+    fontWeight: '700',
+    marginBottom: 15
   },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  image: {
-    width: 150,
-    height: 150,
-    resizeMode: 'contain',
-  },
-  bigText: {
-
-  },
+  availableText: {
+    color: 'blue',
+    position: 'relative',
+    bottom: 50,
+    left: 200,
+    fontSize: 16
+  }
 });
 
 export default HomeScreen
